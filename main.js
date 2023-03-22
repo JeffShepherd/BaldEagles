c002aa86403c58586a4f63f9a8157751152692b8
Jeff S <jeff.shep777@gmail.com>
2023-03-22 09:35:32 -0600

    Add variables to represent canvas coordinates

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+////canvas
 let canvasA = document.getElementById('canvasA')
 const context = canvasA.getContext('2d')
-
+//birdimage
 const eagle = new Image()
 eagle.src = './eagle.png'
 
+////bird coordinate values
+let birdX = 0
+let birdY = 60
 
+////frame rendering
 setInterval(() => {
   context.fillStyle = 'lightgrey'
   context.fillRect(0,0,500,400)
-  context.drawImage(eagle, 0, 60, 20*(281/233), 20)
+  context.drawImage(eagle, birdX, birdY, 20*(281/233), 20)
   //draw image arguements are (image, x cord to place top left of image,
-  //y cord to place top left of image, width to draw image, heigth to draw image )
+  //y cord to place top left of image, width to draw image (multiply by aspect ratio to correct display), heigth to draw image )
 }, 80)
