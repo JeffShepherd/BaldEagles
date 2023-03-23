2bd76cf3e83384c5fdac8aa07bd5edd37b1128b7
Jeff S <jeff.shep777@gmail.com>
2023-03-22 19:20:15 -0600

    Add functionality to reset game when bird hits bottom boundary

--- main.js
+++ main.js
@@ -31,14 +31,28 @@ function addVerticalSpeed() {
   birdVertSpeed = 3
 }
 
+startGame()
 
 //////frame rendering
-setInterval(() => {
-  context.fillStyle = 'lightgrey'
-  context.fillRect(0,0,500,400)
-  birdVertSpeed -= 0.3
-  birdY -= birdVertSpeed
-  context.drawImage(eagle, birdX, birdY, 20*(281/233), 20)
-  //draw image arguements are (image, x cord to place top left of image,
-  //y cord to place top left of image, width to draw image (multiply by aspect ratio to correct display), heigth to draw image )
-}, 80)
+function startGame() {
+  let render = 
+  setInterval(() => {
+    context.fillStyle = 'lightgrey'
+    context.fillRect(0,0,500,400)
+    birdVertSpeed -= 0.3
+    birdY -= birdVertSpeed
+    //draw image arguements are (image, x cord to place top left of image,
+    //y cord to place top left of image, width to draw image (multiply by 
+    //aspect ratio to correct display), heigth to draw image )
+    context.drawImage(eagle, birdX, birdY, 20*(281/233), 20)
+    //check if bird hit bottom
+    if(birdY > 130) {
+      birdVertSpeed = 0
+      birdY = 60
+      clearInterval(render)
+      context.fillRect(0,0,500,400)
+      context.drawImage(eagle, birdX, birdY, 20*(281/233), 20)    }
+  }, 80)
+}
+
+
